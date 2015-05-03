b500caf5743ce0ff581540404ae0d66de98cc1e8
Delio Amaral <A31859@alunos.isel.pt>
2015-05-03 18:53:41 +0100

    added image effects template

--- demo/public/scripts/image_effects.js
+++ demo/public/scripts/image_effects.js
@@ -1,4 +1,46 @@
 var imageEffects = {
-	smoothing: {},
-	sharping: {}
+	adjustment: {
+		autocolor: function() {},
+		autocontrast: function() {},
+		autolevels: function() {}
+	},
+
+	blur_sharpen: {
+		gaussian_blur: function() {},
+		sharpen: function() {}
+	},
+
+	color_correction: {
+		brightness: function() {},
+		contrast: function() {},
+		color_balance: function() {},
+		color_change: {
+			black_white: function() {},
+			sepia: function() {},
+			gray: function() {}
+		}
+	},
+	distort: {
+		fisheye: function() {}
+	},
+	keying: {
+		chromakey: function() {},
+		matte: function() {}
+	},
+	noise_grain: {
+		remove_noise: function() {},
+		add_noise: function() {}
+	},
+	transform: {
+		zoom: function() {},
+		resize: function() {},
+		rotate: function() {},
+		crop: function() {},
+		invert: function() {}
+	},
+	transition: {
+		dissolve: function() {},
+		wipe: function() {},
+		slide: function() {}
+	}
 }
